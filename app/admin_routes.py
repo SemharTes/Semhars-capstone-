4b13b433ab2dea578072f0505a9221e30010c323
Semhar Tes <semhartes7@gmail.com>
2023-02-08 11:28:28 -0500

    refactored admin_routes file and added bcrypt

--- app/admin_routes.py
+++ app/admin_routes.py
@@ -1,8 +1,50 @@
 from flask import Blueprint, request, jsonify, make_response, abort
 from app import db
 from app.models.admin import Admin
+import bcrypt
 
 admin_bp = Blueprint("admin", __name__, url_prefix="/admin")
+
+# register admin
+@admin_bp.route("/register", methods=["POST"])
+def register():
+    admin_name = request.json.get('admin_name', None)
+    password = request.json.get('password', None)
+
+    if not admin_name:
+        return 'Missig admin name!', 400
+    if not password:
+        return 'Missig password!', 400    
+        
+    hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    
+    admin = Admin(admin_name=admin_name, password=hashed.decode("utf8"))
+
+    db.session.add(admin)
+    db.session.commit()
+    return f'welcome {admin_name}'
+
+
+# check login
+@admin_bp.route("/login", methods=["POST"])
+def login():
+    admin_name = request.json.get('admin_name', None)
+    password = request.json.get('password', None)
+
+    if not admin_name:
+        return 'Missig admin name!', 400
+    if not password:
+        return 'Missig password!', 400
+
+    admin = Admin.query.filter_by(admin_name=admin_name).first()
+    if not admin:
+        return 'Admin not found!', 404
+    my_hash = bcrypt.checkpw(password.encode('utf8'), admin.password.encode('utf8'))
+    if my_hash:
+        return f'Welcome back {admin_name}'
+    else:
+        return 'Incorrect Password!'
+
 
 # validate_model
 def validate_model(cls, model_id):
@@ -16,17 +58,6 @@ def validate_model(cls, model_id):
         abort(make_response({"message":f"model id {model_id} not found"}, 404))
     
     return model
-# create admin  
-@admin_bp.route("", methods=["POST"])
-def create_admin():
-    request_body = request.get_json()
-
-    new_admin = Admin.instance_from_json(request_body)
-
-    db.session.add(new_admin)
-    db.session.commit()
-
-    return {"admin":new_admin.to_dict()}, 201
 
 # get all admins
 @admin_bp.route("", methods=["GET"])
